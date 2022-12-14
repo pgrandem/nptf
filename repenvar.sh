15a28b269a726696dfe27d5003b20a62d98bb957
Pierre GRANDEMANGE <pierre.grandemange@cern.ch>
2022-12-14 09:03:30 +0100

    attempt to automatically set current folder as main folder

--- repenvar.sh
+++ repenvar.sh
@@ -16,12 +16,14 @@
 ### rep c++ framework shared objects, functions, data...
 ### "rep" is the top folder. All other paths are referenced to this one.
 ### ----------------------------------------------------------------------------
+#export mainFolder="$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
 export rep=~/private/alpha/rep
 ### relative paths
-export repNamespaces=$rep/cppShare/repNamespaces
-export repObjects=$rep/cppShare/repObjects
+export repNamespaces=$mainFolder/../cppShare/repNamespaces
+export repObjects=$mainFolder/../cppShare/repObjects
 
-
+### update path to use executable everywhere
+export PATH=$mainFolder/build:${PATH}
 
 ### c++ / makefile / ROOT nonsense to make it work
 export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH:+${CPLUS_INCLUDE_PATH}:}$repNamespaces/include:$repObjects/include"
