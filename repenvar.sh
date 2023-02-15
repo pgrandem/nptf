359550e17e64d93088167e9af7f43a86dd35f58b
pgrandem <pierre.grandemange@protonmail.com>
2023-02-15 14:29:41 +0100

    add cppshare submodule

--- repenvar.sh
+++ repenvar.sh
@@ -2,31 +2,33 @@
 ### ************************************************************************ ###
 ### environement variables for a "rep" c++ type project.
 ### Pierre GRANDEMANGE
-### 03/03/2022
+### 15/02/2023
 ### ************************************************************************ ###
 
 
 
-### reference to documentation
+### documentation
 ### ----------------------------------------------------------------------------
 ### export: https://stackoverflow.com/questions/1158091/defining-a-variable-with-or-without-export
 
 
 
 ### rep c++ framework shared objects, functions, data...
-### "rep" is the top folder. All other paths are referenced to this one.
+### "mainFolder" is the top folder. All other paths are referenced to this one.
 ### ----------------------------------------------------------------------------
 #export mainFolder="$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
-export rep=~/private/alpha/rep
-### relative paths
-export repNamespaces=$mainFolder/../cppShare/repNamespaces
-export repObjects=$mainFolder/../cppShare/repObjects
+export mainFolder=$(pwd)
+
+### cppshare git submodule paths
+export cppshare=$mainFolder/cppshare
+export repNamespaces=$cppshare/repNamespaces
+export repObjects=$cppshare/repObjects
 
 ### update path to use executable everywhere
 export PATH=$mainFolder/build:${PATH}
 
 ### c++ / makefile / ROOT nonsense to make it work
-export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH:+${CPLUS_INCLUDE_PATH}:}$repNamespaces/include:$repObjects/include"
+#export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH:+${CPLUS_INCLUDE_PATH}:}$repNamespaces/include:$repObjects/include"
 
 
 
