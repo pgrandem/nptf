d655b97a63f262b5c20a54cf7f668512a3a04eda
pgrandem <pierre.grandemange@protonmail.com>
2023-02-16 11:20:06 +0100

    playing with submodules. Committing with git commit -am

--- repenvar.sh
+++ repenvar.sh
@@ -12,12 +12,10 @@
 ### export: https://stackoverflow.com/questions/1158091/defining-a-variable-with-or-without-export
 
 
-
 ### rep c++ framework shared objects, functions, data...
 ### "mainFolder" is the top folder. All other paths are referenced to this one.
 ### make sure that you have thiroot.sh/$ROOTSYS set
 ### ----------------------------------------------------------------------------
-#export mainFolder="$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
 export mainFolder=$(pwd)
 export data=$mainFolder/data
 
@@ -25,12 +23,7 @@ export data=$mainFolder/data
 export cppshare=$mainFolder/cppshare
 export repNamespaces=$cppshare/repNamespaces
 export repObjects=$cppshare/repObjects
+
 ### update path to use executable everywhere
 export PATH=$mainFolder/build:${PATH}
 
-### c++ / makefile / ROOT nonsense to make it work
-#export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH:+${CPLUS_INCLUDE_PATH}:}$repNamespaces/include:$repObjects/include"
-
-
-
-$CP	
